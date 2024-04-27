bb3633f51fb4ce8696b404189a73d9bc522295f1
Shaaaaaaaaark <bella921127@gmail.com>
2024-04-27 13:15:06 +0800

    re

---
+++ hw4/norace.c
@@ -0,0 +1,48 @@
+﻿#include <stdio.h>
+#include <pthread.h>
+
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+#define LOOPS 1000000
+int money = 1000;
+
+void *inc()
+{
+  for (int i=0; i<LOOPS; i++) {
+    pthread_mutex_lock( &mutex1 );
+    money = money + 100;
+    pthread_mutex_unlock( &mutex1 );
+  }
+  return NULL;
+}
+
+void *dec()
+{
+  for (int i=0; i<LOOPS; i++) {
+    pthread_mutex_lock( &mutex1 );
+    money = money - 100;
+    pthread_mutex_unlock( &mutex1 );
+  }
+  return NULL;
+}
+
+int main() 
+
+{
+    int counter=0;
+    while (1)
+    {
+       counter++;
+       pthread_t thread1, thread2;
+
+       pthread_create(&thread1, NULL, inc, NULL);
+       pthread_create(&thread2, NULL, dec, NULL);
+
+       pthread_join( thread1, NULL);
+       pthread_join( thread2, NULL);
+       if (counter==1000){
+        printf("money=%d\n",money);
+        printf("No Errors Heppened");
+        break;
+       }
+    }
+}
---
+++ hw4/race.c
@@ -0,0 +1,48 @@
+﻿#include <stdio.h>
+#include <pthread.h>
+
+#define LOOPS 1000000
+int money = 1000;
+
+void *inc()
+{
+  for (int i=0; i<LOOPS; i++) {
+    money = money + 100;
+  }
+  return NULL;
+}
+
+void *dec()
+{
+  for (int i=0; i<LOOPS; i++) {
+    money = money - 100;
+  }
+  return NULL;
+}
+
+
+int main() 
+{
+    int counter=0;
+    while (1)
+    {
+        pthread_t thread1, thread2;
+
+        pthread_create(&thread1, NULL, inc, NULL);
+        pthread_create(&thread2, NULL, dec, NULL);
+
+        pthread_join( thread1, NULL);
+        pthread_join( thread2, NULL);
+        if(money<0){
+            printf("Error");
+            break;
+        }
+        else
+        {
+            printf("money=%d\n", money);
+        }
+        
+    }
+    
+ 
+}
